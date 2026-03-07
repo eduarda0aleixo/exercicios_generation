2ba43d0bfae5c705adeef61a09dfcb55a75bae4e
EduAleixo <eduarda.aleixo07@gmail.com>
2026-03-06 23:48:23 -0300

    Atividades 06.03.2026 de Fila e Pilha

--- estrutura_dados/exercicioQueue.ts
+++ estrutura_dados/exercicioQueue.ts
@@ -1,4 +1,5 @@
 import { Queue } from "./Queue";
+import * as readline from 'readline-sync';
 
 const fila = new Queue<string>();
 let continuar: boolean = false;
@@ -10,6 +11,15 @@ do{
         1 - Adicionar Cliente na Fila
         2 - Listar todos os Clientes
         3 - Retirar Clientes da Fila
-        4 - Sair\n`);
+        0 - Sair\n`);
+        opcao = readline.questionInt("Digite a opcao desejada: ");
+
+    switch(opcao) {
+        case 1:
+            const nome = readline.question("Digite o nome do cliente: ");
+            fila.enqueue(nome);
+            console.log(`Cliente ${nome} adicionado à fila!`);
+            break;
+    }
 
 }while(continuar)
